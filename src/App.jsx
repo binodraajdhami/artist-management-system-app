97b63b59ac583f06cadc99da6a8ed602726dde1a
Binod Raj Dhami <binodraaz@gmail.com>
2024-08-23 11:39:59 +0545

    project setup

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+
+function App() {
+	return (
+		<>
+			<div>
+				<a href="#" target="_blank"></a>
+				<a href="#" target="_blank">
+					<img
+						src={reactLogo}
+						className="logo react"
+						alt="Artist Management System"
+					/>
+				</a>
+			</div>
+			<h1>Artist Management System</h1>
+		</>
+	);
+}
+
+export default App;

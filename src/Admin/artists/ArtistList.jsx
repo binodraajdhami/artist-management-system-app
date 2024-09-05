83ded871954426ffc56b83773d26ce64905902c0
Binod Raj Dhami <binodraaz@gmail.com>
2024-09-05 22:40:35 +0545

    completed export csv file

--- src/Admin/artists/ArtistList.jsx
+++ src/Admin/artists/ArtistList.jsx
@@ -94,6 +94,21 @@ export default function ArtistList() {
 			.then((data) => {
 				setExporting(false);
 				notify.showSuccess("Export Successful!");
+
+				const blob = new Blob([data.data], { type: "text/csv" });
+
+				// Create a URL for the Blob
+				const url = URL.createObjectURL(blob);
+
+				// Create an anchor tag for downloading
+				const a = document.createElement("a");
+
+				// Set the URL and download attribute of the anchor tag
+				a.href = url;
+				a.download = "download.csv";
+
+				// Trigger the download by clicking the anchor tag
+				a.click();
 			})
 			.catch((err) => {
 				setExporting(false);
